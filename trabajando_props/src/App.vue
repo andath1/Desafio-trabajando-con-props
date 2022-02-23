09e18c1d44b4f542a6a5dca949a276fa4dcbf042
andres rojas <andath1@hotmail.com>
2022-02-23 16:40:07 -0300

    props

---
+++ trabajando_props/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <br>
+  <form class="form">
+    <label for="todoitem">Tarea</label>
+    <input type="text" v-model="input" id="todoitem" placeholder="Ingresar una nueva tarea"/>
+    <button type="button" class="btn" v-on:click.prevent="add()"><span>Añade</span> </button>
+  </form>
+  <ItemsCopia :tareas="this.tareas" @deleteItem="deleteElement"/>
+</template>
+<script>
+import ItemsCopia from "./components/ItemsCopia.vue";
+export default {
+  name: 'App',
+  components: {
+    ItemsCopia,
+  },
+  data(){
+    return {
+      tareas: [],
+      input: ""
+    }
+  },
+  methods: {
+    add(){
+      this.tareas.push(this.input);
+      this.input = "";
+    },
+    deleteElement(index){
+      this.tareas.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style>
+label{
+  font-size: 30px;
+  margin-right: 20px;
+  margin-left: 20px;
+  
+}
+
+.form{
+  margin-left: 400px;
+  margin-bottom: 50px;
+}
+li{
+  margin-left: 450px;
+  margin-bottom: 10px;
+  font-size: 20px;
+
+}
+.btn {
+    background-color: white;
+    border-radius: 10px;
+    margin-left: 10px;
+    border: 4px solid #4e87f1;
+}
+</style>
---
+++ trabajando_props/src/components/ItemsCopia.vue
@@ -0,0 +1,29 @@
+<template>
+    
+        <ul>
+            <li v-for="(tarea,index) in tareas" :key="index">
+                {{tareas[index]}}
+                <button @click="deleteItem(index)">Eliminar</button>
+            </li>
+        </ul>
+</template>
+
+<script>
+export default ({
+    name: "ItemsCopia",
+    props: {
+        tareas: Array
+    },
+    methods: {
+        deleteItem(index){
+            this.$emit('deleteItem', index)
+        }
+    }
+})
+</script>
+
+<style>
+button{
+    margin-left: 20px;
+}
+</style>
